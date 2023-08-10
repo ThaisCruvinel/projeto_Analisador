6d2361f4b4cdd018432a73d30416885a26af94a6
Thaís Cruvinel <thaiscruvinel14@gmail.com>
2023-08-10 14:36:21 -0300

    Adicionando arquivos

---
+++ js/mult.js
@@ -0,0 +1,11 @@
+function calcular(){
+    var tn1 = document.getElementById('txtn1');
+    var tn2 = document.getElementById('txtn2');
+    var resul = document.getElementById('resul');
+    var n1 = Number(tn1.value);
+    var n2 = Number(tn2.value);
+
+    var mult = n1 * n2;
+
+    resul.innerText = `A multiplicação dos números ${n1} e ${n2} é igual a ${mult}`;
+}
